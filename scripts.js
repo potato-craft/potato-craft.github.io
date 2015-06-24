6c9c0ee53d774103dc550360e245ca1fb30a2d60
potato-craft <jimmy.moore.potato@gmail.com>
2015-06-24 13:14:33 -0400

    scope.todos

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 var app = angular.module('Todo', []);
 app.controller('TodoCtrl', function($scope) {
-  $scope.message = 'Angular is pretty cool!';
+  $scope.todos = [
+    'Learn Sketch', 
+    'Look at Dribbble and feel inferior',
+    'Actually learn how to use the Pen tool'
+  ];
 });
